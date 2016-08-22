19ce047196ba723c4c42014b8611d68654f0e0ef
CarlaAstudillo <carla.a.astudillo@gmail.com>
2016-08-22 02:56:35 -0400

    added header & started legend

--- init.js
+++ init.js
@@ -15,6 +15,92 @@ var strokeColor = '#444';
     	.style('left', poptartSize + 'px')
     	.style('padding', padding + 'px');
 
+
+
+//Create the legend //
+
+      var legendWidth = 900;
+
+      
+      
+
+      var legendPopTarts = legend.append('g')
+        .attr('transform', 'translate(' + legendWidth/ 8 + ',0)')
+        .selectAll('g')
+        .data(["HEIGHT OF POP-TART = KEVIN'S SCORE ", "WIDTH OF POP-TART = DEMI'S SCORE"])
+        .enter().append('g')
+        .attr('transform', function(d, i) {
+          var x = i * (poptartSize * 1.6);
+          return 'translate(' + [x, 0] + ')';
+        });
+
+        legendPopTarts.append("rect")
+        .classed('poptartlegend', true)
+        .attr("width", 70)
+        .attr("height", 70)
+        .attr('transform', function(d, i) {
+          var x = (i % 2) - 70/2;
+          var y = 27;
+          return 'translate(' + [x, y] +
+            ')rotate(-55, 45, 30)';
+        })
+        .style("stroke", '#444')
+        .style("opacity", 0.7)
+        .style("fill", 'none')
+        .style("stroke-width", 2)
+
+        legendPopTarts.append("rect")
+        .classed('frostlegend', true)       
+    .style("stroke", "#444")
+    .style("stroke-width", 1)  
+    .style("fill", "none")
+    .style("opacity", 0.9)
+    .attr('transform', function(d, i) {
+          var x = (i % 2) - 70/2;
+          var y = 27;
+          return 'translate(' + [x, y] +
+            ')rotate(-55, 45, 30)';
+        })
+    .attr("x", 10)
+     .attr("y", 10)
+    .attr("width", 50)
+        .attr("height", 50)
+
+
+        legendPopTarts.append("svg:image")
+    .attr("xlink:href", "ruler.svg")
+    .attr('transform', function(d, i) {
+          var x = (i % 2) - 130/2;
+          var y = 27;
+          return 'translate(' + [x, y] +
+            ')';
+        })
+    .attr('transform', function(d, i) {
+          if (i == 1){
+            return 'rotate(260, 45, 26)'
+
+            }
+            else{
+          return 'rotate(-190, 6, 25)';
+          }
+        })
+    .attr("width", 130)
+    .attr("height", 85)
+    .attr("x", -20)
+    .attr("y",-15);
+
+
+//         d3.xml("ruler.svg", "image/svg+xml", function(xml) {
+  
+// });
+
+        legendPopTarts.append('text')
+        .attr('y', 135)
+        .attr('text-anchor', 'middle')
+        .attr('fill', '#444')
+        .style('font-size', '12px')
+        .text(function(d) {return d});
+
  
 var sprinklescolor = d3.scaleOrdinal(d3.schemeCategory10);
 
@@ -95,7 +181,7 @@ var frostscale = d3.scaleQuantize().domain([minimdb, maximdb]).range(frostcolors
         }).style('top', function(d, i) {
           return Math.floor(i / 5) * poptartSize * 1.7 + (d.demi_score * 10) + 60 + 'px';
         }).html(function(d) {
-          return '<b>' + d.episode +                          // closing </a> tag
+          return '<b>' + d.episode +                         
         "</b><br/><div class='scores'>KEVIN SCORE: " + d.kevin_score + "<br>DEMI SCORE: " + d.demi_score + "<br>IMDB SCORE: " + d.imdb_score + '</div>'
         });
 
@@ -143,10 +229,8 @@ var frostscale = d3.scaleQuantize().domain([minimdb, maximdb]).range(frostcolors
       .attr('transform', function(d, i) {
           var x = (i % 5) * poptartSize * 1.25;
           var y = Math.floor(i / 5) * poptartSize * 1.7;
-          var test = [(d.kevin_score * 10) - (d.kevin_score * 7)]/2
-          var test2 = [(d.demi_score * 10) - (d.demi_score * 7)]/2
-          console.log(test2)
-          return 'translate(' + [x + test , y - test ] +
+          var border = [(d.kevin_score * 10) - (d.kevin_score * 7)]/2
+          return 'translate(' + [x + border , y - border ] +
             ')rotate(-55,54, 25)';
         })
         .each(drawRandomSprinkles);
